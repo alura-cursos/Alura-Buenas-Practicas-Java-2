a06e87e36de56c8f6b5d90e5adee4bef3a69b1da
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2024-08-11 19:07:39 -0300

    V1.2

--- src/main/java/com/aluracursos/adopet/api/exceptions/ValidacionException.java
+++
@@ -1,8 +0,0 @@
-package com.aluracursos.adopet.api.exceptions;
-
-public class ValidacionException extends RuntimeException {
-
-    public ValidacionException(String message) {
-        super(message);
-    }
-}
