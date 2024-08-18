8dfe40c06fbaac152cbda49ef07f1eec3c60c724
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2024-08-18 18:23:26 -0300

    V2.2 V2

--- src/main/java/com/aluracursos/adopet/api/dto/ReprobacionAdopcionDTO.java
+++ src/main/java/com/aluracursos/adopet/api/dto/ReprobacionAdopcionDTO.java
@@ -1,4 +1,4 @@
 package com.aluracursos.adopet.api.dto;
 
-public record ReprobacionAdopcionDTO(Long idSolicitud, String justificacion) {
+public record ReprobacionAdopcionDTO(Long idAdopcion, String justificacion) {
 }
