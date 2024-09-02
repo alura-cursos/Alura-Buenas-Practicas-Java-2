74a30e7ae92e255bbfbbc664f242d50873e09a79
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2024-09-02 08:42:34 -0300

    Cambios para el proximo curso

---
+++ src/main/java/com/aluracursos/adopet/api/service/EmailServiceDev.java
@@ -0,0 +1,11 @@
+package com.aluracursos.adopet.api.service;
+
+public class EmailServiceDev implements EmailService{
+
+    public void enviarEmail(String to, String subject, String message) {
+        System.out.println("Enviando email fake");
+        System.out.println("Destinatario: " +to);
+        System.out.println("Asunto: " +subject);
+        System.out.println("Mensaje: " +message);
+    }
+}
